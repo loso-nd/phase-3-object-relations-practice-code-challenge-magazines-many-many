745135d3336b109def162a8eeb11dd5b87219d1b
Nahum <losond8@gmail.com>
2021-07-14 09:22:01 -0700

    Completed the AR portions including but not limited to migration tables, models, and seed list. Finally step to complete Aggregate methods.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
     has_many :roles
     has_many :actors, through: :roles
+
+    def cast_role(actor, character_name, salary)
+        Role.create(actor_id: actor.id, character_name: "character_name", salary: salary)
+    end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,10 @@
 class Role < ActiveRecord::Base
     belongs_to :actor #belongs_to method and receives a symbol representing an actor 
     belongs_to :movie
+
+    def credit
+        "#{self.character_name}: Played by #{actor.name}"
+    end
 end
 
 
